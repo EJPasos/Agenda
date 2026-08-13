0c8642d9a974b3cdd9341cf28dfd2b8f7411afcc
Julian Pasos <jpasos@uabc.edu.mx>
2026-08-12 22:13:16 -0700

    Se actualiza module-info.java para abrir y exportar el paquete com.example.agenda.view

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,14 @@
 module com.example.agenda {
     requires javafx.controls;
     requires javafx.fxml;
-    requires java.sql;
 
+    requires java.sql;
 
     opens com.example.agenda to javafx.fxml;
     exports com.example.agenda;
+
+    opens com.example.agenda.view to javafx.fxml;
+    exports com.example.agenda.view;
+
     exports com.example.agenda.model;
-    opens com.example.agenda.model to javafx.fxml;
 }
