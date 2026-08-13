83e486f383e2a092f50f6a55bb7581324724ca0f
Julian Pasos <jpasos@uabc.edu.mx>
2026-08-12 19:36:29 -0700

    Refactor project structure and database connection implementation

--- src/main/java/com/example/agenda/App.java
+++ src/main/java/com/example/agenda/App.java
@@ -7,11 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene();
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.agenda {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.agenda to javafx.fxml;
     exports com.example.agenda;
+    exports com.example.agenda.model;
+    opens com.example.agenda.model to javafx.fxml;
 }
